022c6e86b8d1850d1cc8c7762d40dcff556b4e18
Gokarna Shrestha <gokarnashrestha15@gmail.com>
2026-02-09 15:08:14 +0545

    uploading

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -9,7 +9,7 @@ const Features = () => {
     <SectionContainer>
       <div
         className="
-          relative
+          relative top-15
           max-w-150
           tablet:max-w-200
           laptop:max-w-260
